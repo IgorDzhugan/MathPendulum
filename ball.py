bc5568f1ebdc8ee70b1e1d8a76f3d349bdd76144
IgorDzhugan <109528899+IgorDzhugan@users.noreply.github.com>
2022-09-10 00:03:09 +0300

    Add files via upload

---
+++ ball.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+import numpy as np
+
+
+class Ball:
+
+    def __init__(self, canvas, wind_size, size=20, length=3, alpha=75, mass=1, gravity=9.8):
+        self.rad = size
+        self.length = length
+        self.alpha = np.radians(alpha)
+        self.x = wind_size / 2
+        self.y = wind_size * 0.5
+        self.center = wind_size / 2
+        self.circle = canvas.create_oval(int(self.x - self.rad),
+                                         int(self.y - self.rad),
+                                         int(self.x + self.rad),
+                                         int(self.y + self.rad))
+        self.m = mass
+        self.g = gravity
+        self.w = np.sqrt(self.g / self.length)
+        self.time = 0
+
+    def move(self, canvas, root):
+        x = self.center + self.length * 50 * np.sin(self.alpha*np.cos(self.w * self.time))
+        y = self.center + self.length * 50 * np.cos(self.alpha*np.cos(self.w * self.time))
+        print(x, y)
+        canvas.move(self.circle, x - self.x, y - self.y)
+        self.x = x
+        self.y = y
+        root.update()
+        self.time += 0.01
+
+    def function(self, y, t):
+        mu = 0.1
+        return()
---
+++ main.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+import ball
+from time import sleep
+
+
+if __name__ == '__main__':
+    wind_size = 700
+    root = tk.Tk(className='Маятник')
+    root.geometry(str(wind_size) + 'x' + str(wind_size))
+    canvas = tk.Canvas(root, bg='white', height=wind_size, width=wind_size)
+    canvas.pack()
+    ball = ball.Ball(canvas, wind_size)
+    x, y = 20, 250
+    while True:
+        ball.move(canvas, root)
+        sleep(0.03)
+    root.mainloop()
+
+
+
