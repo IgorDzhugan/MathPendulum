06950fb6494d4184be1d71fcb18b1cd44363ef74
IgorDzhugan <109528899+IgorDzhugan@users.noreply.github.com>
2022-09-10 00:04:14 +0300

    Update ball.py

--- ball.py
+++ ball.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class Ball:
 
-    def __init__(self, canvas, wind_size, size=20, length=3, alpha=75, mass=1, gravity=9.8):
+    def __init__(self, canvas, wind_size, size=20, length=150, alpha=75, mass=1, gravity=9.8):
         self.rad = size
         self.length = length
         self.alpha = np.radians(alpha)
@@ -21,8 +21,8 @@ class Ball:
         self.time = 0
 
     def move(self, canvas, root):
-        x = self.center + self.length * 50 * np.sin(self.alpha*np.cos(self.w * self.time))
-        y = self.center + self.length * 50 * np.cos(self.alpha*np.cos(self.w * self.time))
+        x = self.center + self.length * np.sin(self.alpha*np.cos(self.w * self.time))
+        y = self.center + self.length * np.cos(self.alpha*np.cos(self.w * self.time))
         print(x, y)
         canvas.move(self.circle, x - self.x, y - self.y)
         self.x = x
